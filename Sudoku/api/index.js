feb854f15bb5e650808f9761ecbfcabadd174311
Jose Delgado Matamoros <alonsodelm@gmail.com>
2017-10-02 10:17:24 -0600

    First commit

--- Sudoku/api/index.js
+++ Sudoku/api/index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/sudoku', { useMongoClient: true})
