e92206ec0b3247a165783cc3c4f47df1a6e61d0a
Jose Delgado Matamoros <alonsodelm@gmail.com>
2017-10-06 16:38:04 -0600

    Change for Heroku

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://jose:macros98@ds159344.mlab.com:59344/sudoku', { useMongoClient: true})
+mongoose.connect('mongodb://localhost:27017/curso_mean', { useMongoClient: true})
     .then(() => {
         console.log('La conexion a la base de datos se realizo correctamente...');
         app.listen(port, () => {
-            console.log("El servidor local estan corriendo...");
+            console.log("El servidor local estan corriendo en localhost "+port);
         });
     })
     .catch(err => console.log(err));
